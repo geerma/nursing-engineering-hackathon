854f7b8ebb2acb0fea9123a9ad6669d671f8a552
moe-ebrahimi <mohamad.ebrahimi@ucalgary.ca>
2023-03-04 13:53:11 -0700

    notification user added

--- Web/src/pages/Notificationspage/Notificationspage.jsx
+++ Web/src/pages/Notificationspage/Notificationspage.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Notificationspage.css"
+import { VscAccount } from "react-icons/vsc";
+import newUsers from "./newusers.json"
 
 const Notificationspage = () => {
-  return (
-    
 
-  <div id="inx9u" class="gjs-row">
-      <div class="gjs-cell" id="ignxt">
+  return (
+  <div id="inx9u" className="gjs-row">
+      <div className="gjs-cell" id="ignxt">
         <h2>New Users</h2>
+        {
+          newUsers.map((item) => (
+              <div className="gjs-row" id="iivl" key={item.id}>
+                <div className="gjs-cell" id="ixzi">
+                    <VscAccount id='icon'/>
+                </div>
+                <div className="gjs-cell" id="igqj">
+                <p>{item.first_name} { item.last_name}</p>
+                </div>
+              </div>
+          ))
+        }
     </div>
-      <div class="gjs-cell" id="ic6b">
+      <div className="gjs-cell" id="ic6b">
         <h2>Update on system</h2>
     </div>
-      <div class="gjs-cell" id="i1fco">
-        <h2>Update of Education</h2>
+      <div className="gjs-cell" id="i1fco">
+        <h2>Update on Education</h2>
     </div>
   </div>
-
 
   )
 }
