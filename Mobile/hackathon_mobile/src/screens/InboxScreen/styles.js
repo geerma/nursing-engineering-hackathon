44493e980250fce31be3f5153b5499de2f74101a
geerma <geerm1997@gmail.com>
2023-03-04 14:48:07 -0700

    Inbox done

---
+++ Mobile/hackathon_mobile/src/screens/InboxScreen/styles.js
@@ -0,0 +1,57 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  screen: {
+    alignItems: "center",
+  },
+
+  question: {
+    alignItems: "center",
+    margin: 8,
+  },
+
+  header: {
+    fontSize: 30,
+  },
+
+  button: {
+    backgroundColor: "#424BB2",
+    marginLeft: 26,
+    marginRight: 26,
+    marginTop: 20,
+    height: 40,
+    width: 300,
+    borderRadius: 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  choiceButton: {
+    backgroundColor: "#424BB2",
+    marginLeft: 26,
+    marginRight: 26,
+    marginTop: 10,
+    height: 22,
+    width: 300,
+    borderRadius: 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  choiceTitle: {
+    color: "white",
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+
+  buttonTitle: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  chatheader: {
+    fontSize: 20,
+  }
+
+});
