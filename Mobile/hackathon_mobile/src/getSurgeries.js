18b6b2ca9367afec819c16781e66554ffd1bdd6e
usmanzia98 <46509502+usmanzia98@users.noreply.github.com>
2023-03-04 14:35:57 -0700

    Update getSurgeries.js

--- Mobile/hackathon_mobile/src/getSurgeries.js
+++ Mobile/hackathon_mobile/src/getSurgeries.js
@@ -1,21 +1,21 @@
-import { auth, db } from "../../firebase/config";
-import { doc, getDoc } from "firebase/firestore";
+// import { auth, db } from "../../firebase/config";
+// import { doc, getDoc } from "firebase/firestore";
 
 
-const getSurgeries = async () => {
-    const docRef = doc(db, "information", "gbVf8nsPkBzSMbUOKUHL");
-    const docSnap = await getDoc(docRef);
+// const getSurgeries = async () => {
+//     const docRef = doc(db, "information", "gbVf8nsPkBzSMbUOKUHL");
+//     const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-      const surgeriesData = docSnap.data();
+//     if (docSnap.exists()) {
+//       const surgeriesData = docSnap.data();
 
-      console.log(informationData);
-      setSurgeries(surgeriesData);
-    } else {
+//       console.log(informationData);
+//       setSurgeries(surgeriesData);
+//     } else {
 
-      console.log("No such information");
-    }
-  };
-useEffect(() => {
-    getSurvey();
-  }, []);
+//       console.log("No such information");
+//     }
+//   };
+// useEffect(() => {
+//     getSurvey();
+//   }, []);
