7ebb43e1c2296c00239c515e67c6729f366a33ab
geerma <geerm1997@gmail.com>
2023-03-04 10:16:16 -0700

    Add website initial page files

---
+++ Web/src/pages/Educationpage/Educationpage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Educationpage = () => {
+  return (
+    <div>Educationpage</div>
+  )
+}
+
+export default Educationpage
---
+++ Web/src/pages/Notificationspage/Notificationspage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notificationspage = () => {
+  return (
+    <div>Notificationspage</div>
+  )
+}
+
+export default Notificationspage
