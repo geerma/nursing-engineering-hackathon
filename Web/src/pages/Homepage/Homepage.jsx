7dd6e859f6c52975440d8abf5d0ecf1243d17cc2
k-reed <104884586+k-reed@users.noreply.github.com>
2023-03-04 14:07:00 -0700

    fixed homepage and search page

--- Web/src/pages/Homepage/Homepage.jsx
+++ Web/src/pages/Homepage/Homepage.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import "./HomePage.css"
+import "./Homepage.css"
 import NavBar from "../../components/Navbar/Navbar"
 
 
 function Homepage() {
   return (
     <>
-      <div className='Homepage'></div>
-      <NavBar />
+      <div className='homepage-container'>
+        <div className='navbar'>
+          <NavBar />
+        </div>
+        <div className='homepage-content'>
+          <h1>Welcome to the My Health Information Main Page</h1>
+          <p>News items, videos, or other content goes here.</p>
+        </div>
+      </div>
     </>
 
   )
